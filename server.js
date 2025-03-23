93b978ab23ebbb1883bbaf9a3f50a57a7ecf0492
PrnvCodr <pranavkashyap122@gmail.com>
2025-03-23 18:33:27 +0530

    feat: added the Env file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use("/public", express.static(__dirname + "/public"))
+const PORT = process.env.PORT || 1337; // Must use process.env.PORT for Vercel
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
-app.listen(1337, () => {
-  console.log("The server is up and running!");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
